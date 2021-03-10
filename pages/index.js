5937cc2bed7b3c08111776cef50dd45759edda50
will0684 <jordan.walcaraz@gmail.com>
2021-03-10 13:47:07 -0500

    Added redirect from homepage on email confirmation

--- pages/index.js
+++ pages/index.js
@@ -44,6 +44,17 @@ export default function Home({ allPostsData }) {
           ))}
         </ul>
       </section>
+      <script>
+        if (window.netlifyIdentity) {
+          window.netlifyIdentity.on("init", user => {
+            if (!user) {
+              window.netlifyIdentity.on("login", () => {
+                document.location.href = "/admin/";
+              });
+            }
+          })
+        }
+      </script>
     </Layout>
   )
 }
