ea666b4eace7bac9c1d3b1ded5b60b30ff4d6f10
will0684 <jordan.walcaraz@gmail.com>
2021-03-10 16:06:17 -0500

    modified date formatting

--- components/date.js
+++ components/date.js
@@ -1,6 +1,6 @@
-import { parseISO, format } from 'date-fns'
+import { format, formatISO, parseISO } from 'date-fns'
 
 export default function Date({ dateString }) {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
+  const dateISO = parseISO(dateString)
+  return <time dateTime={formatISO(dateISO)}>{format(dateISO, 'LLLL d, yyyy')}</time>
 }
--- pages/index.js
+++ pages/index.js
@@ -31,17 +31,19 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date date={date} />
-              </small>
-            </li>
-          ))}
+          {allPostsData[0] === undefined ? null : 
+            allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>
+                  <a>{title}</a>
+                </Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            ))
+          }
         </ul>
       </section>
     </Layout>
