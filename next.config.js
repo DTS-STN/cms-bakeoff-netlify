9710ad892e9a5ec73a3ed58f4845788e4ade0e3d
will0684 <jordan.walcaraz@gmail.com>
2021-03-10 13:04:33 -0500

    remove import

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-const withOptimizedImages = require('next-optimized-images');
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 
-module.exports = withOptimizedImages({
-  /* config for next-optimized-images */
+module.exports = withPlugins([
+  [optimizedImages, {
+    /* config for next-optimized-images */
+  }],
 
-  // your config for other plugins or the general next.js here...
-});
+  // your other plugins here
+
+]);
