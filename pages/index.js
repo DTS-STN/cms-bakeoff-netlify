fb3cfffd48e67c2f6d864c831d08d311ab6ec52b
will0684 <jordan.walcaraz@gmail.com>
2021-03-15 16:04:59 -0400

    merge language-toggle

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
-import Head from "next/head";
-import React, { useState, useEffect } from "react";
-import Layout, { siteTitle } from "../components/layout";
-import ToggleButton from "../components/toggleButton";
-import utilStyles from "../styles/utils.module.css";
-import { getSortedPostData } from "../lib/posts";
-import Link from "next/link";
-import Date from "../components/date";
-import { useRouter } from "next/router";
-
-
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+import { useRouter } from 'next/router'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostData();
@@ -31,42 +27,24 @@ export async function GetStaticPaths() {
 }
 
 export default function Home({ allPostsData }) {
-  const [language, setLanguage] = useState("en");
-  const router = useRouter();
 
-  const toggleLanguage = (language) => {
-    console.log(language);
-    language === "fr"
-      ? router.push("/", "/", { locale: "en" })
-      : router.push("/", "/", { locale: "fr" });
-  };
-
-  useEffect(() => {
-    if ((router.locale === "fr") | window.location.pathname.includes("/fr")) {
-      setLanguage("fr");
-    } else {
-      setLanguage("en");
-    }
-  }, [toggleLanguage]);
+  const router = useRouter()
 
   const languageFilteredPosts = () => {
-    return allPostsData[0] === undefined
-      ? null
-      : allPostsData.map(({ id, date, title , lang}) => {
-          if (lang === language)
-            return (
-              <li className={utilStyles.listItem} key={id}>
-                <Link href={`${lang}/posts/${id}`}>
-                  <a>{title}</a>
-                </Link>
-                <br />
-                <small className={utilStyles.lightText}>
-                  <Date dateString={date} />
-                </small>
-              </li>
-            );
-        });
-  };
+    return allPostsData[0] === undefined ? null : 
+    allPostsData.map(({ id, date, title, lang }) => {if (lang === router.locale) return(
+      <li className={utilStyles.listItem} key={id}>
+        <Link href={`/posts/${id}`}>
+          <a>{title}</a>
+        </Link>
+        <br />
+        <small className={utilStyles.lightText}>
+          <Date dateString={date} />
+        </small>
+      </li>
+    )
+  })
+  }
 
   return (
     <Layout home>
@@ -74,7 +52,6 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
-      <ToggleButton language={language} toggleLanguage={toggleLanguage} />
       <section className={utilStyles.headingMd}>
         <p>Web dev learning Next.js</p>
         <p>
